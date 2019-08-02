096ba61016f932c331bf83379a5c57e05fe70334
Matheus2001 <35998667+Matheus2001@users.noreply.github.com>
2019-08-02 09:26:30 -0300

    Add files via upload

---
+++ entidades/aluno.py
@@ -0,0 +1,6 @@
+class Aluno():
+    def __init__(self, id_aluno,nome, endereco, matricula, cpf):
+        self.nome = nome
+        self.endereco = endereco
+        self.matricula = matricula
+        self.cpf = cpf
